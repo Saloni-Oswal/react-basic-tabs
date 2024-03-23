ca726c785609e7252891dfae935a4d6b15de3400
Saloni Oswal <salonioswal22@gmail.com>
2024-03-23 19:00:29 -0400

    Optimised logic

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -13,21 +13,11 @@ const Tabs: React.FC<PropsWithChildren> = ({
 
   if (!Array.isArray(children)) return <>{children}</>;
 
-  const updatedChildren = children.map(
-    (child: React.ReactElement, index: number) => {
-      return React.cloneElement(child, {
-        tabIndex: index,
-      });
-    }
-  );
-
   return (
     <div className='tabs-container'>
-      {updatedChildren.map((child: React.ReactElement, index: number) => {
-        const {
-          tabIndex,
-          children,
-        }: { tabIndex: number; children: React.ReactElement } = child.props;
+      {children.map((child: React.ReactElement, index: number) => {
+        const { children }: { tabIndex: number; children: React.ReactElement } =
+          child.props;
         return (
           <div className='tabs'>
             <TabButton
@@ -37,7 +27,7 @@ const Tabs: React.FC<PropsWithChildren> = ({
               isActive={activeTab === index}
               {...child.props}
             ></TabButton>
-            {tabIndex === activeTab ? children : ""}
+            {index === activeTab ? children : ""}
           </div>
         );
       })}
